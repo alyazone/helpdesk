cd2bf6d10e0b36323a53c0bcdee4d5de1b08b981
alyazone <alyamaisarazuan@gmail.com>
2025-12-30 08:16:30 +0800

    move files to migrations folder

--- add_tingkat_to_users.sql
+++
@@ -1,8 +0,0 @@
--- Migration: Add tingkat column to users table
--- Run this if you already have an existing database
-
-USE helpdesk_db;
-
--- Add tingkat column to users table
-ALTER TABLE users
-ADD COLUMN tingkat VARCHAR(50) AFTER no_sambungan;
